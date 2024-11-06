7b8901cfa426cfb2c6749b87bd54f12b37291fbf
QuentinGouttaya <beatsgr3y@gmail.com>
2024-11-06 14:38:47 +0100

    working dockerfile commit, fixed entrypoint for initialisation of app

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,13 @@ composer install --no-interaction
 # Generate Laravel application key
 php artisan key:generate
 
-php migrate:fresh --seed
+php artisan migrate:fresh --seed
 
 # Install Node.js dependencies
 npm install
 
 # Build frontend assets
 npm run build
-
 
 # Start Apache in the foreground
 apachectl -D FOREGROUND
